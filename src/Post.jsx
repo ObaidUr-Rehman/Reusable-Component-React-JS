efe2763e5a354038cb43e738c27aa1089bf8c078
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-10-15 14:38:01 +0300

    project done

---
+++ src/Post.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Post = (props) => {
+  return (
+      <>
+      <div className='main'>
+
+      <div className='about-post'>
+            <div className='pro-img'>
+                <img src={props.image1} alt="profile Iamge" />
+            </div>
+
+            <div className='info'> 
+                <p className='name'>{props.name}</p>
+                <p className='time'>9h🌐</p>
+             </div>
+            
+           
+        </div>
+
+        <div className='para'>
+            <p><b>{props.caption}</b></p>
+        </div>
+
+
+        <div className='postimg'>
+            <img src={props.image} alt="" />
+        </div>
+
+        <div className='reaction'>
+            <h3>❤😢👍 <strong>45</strong> </h3>
+            <div className='comment'>
+            <h5>170 Comment .</h5>
+            <h5> 16 Share</h5>
+            </div>
+        </div>
+<hr />
+        <div className='my-btn'>
+            <button> 👍Like</button>
+            <button> 💬Comment</button>
+            <button> ↪ Share</button>
+        </div>
+
+
+    
+
+
+
+      </div>
+      </>
+  )
+}
+
+export default Post;
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Post from './Post';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <>
+    <Post image1="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeeUl9IZDN97pBQNgeunx6dD1df-4g7vkPFw&usqp=CAUhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeeUl9IZDN97pBQNgeunx6dD1df-4g7vkPFw&usqp=CAU" image="https://png.pngtree.com/png-vector/20211015/ourlarge/pngtree-t20-world-cup-trophy-vactor-and-png-image-design-png-image_3984192.png" name="Obaid Ur Rehman"
+    caption="T20 Cricket World Cup Trophy 2022"
+    ></Post>
+
+
+    <Post image1="https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg" image="https://resources.pulse.icc-cricket.com/ICC/photo/2021/08/15/ca610e1e-e4aa-4efb-aa00-f2ad6478ee3e/Lead-image-Website-article.jpeg" name="Ahsan Omerjee" caption="All Winning Captain Of T20 World Cup"
+    ></Post>
+
+    <Post image1="https://img.freepik.com/premium-vector/young-man-avatar-character-vector-illustration-design_24877-18516.jpg?w=2000" image="https://www.pcb.com.pk/timthumb.php?src=images/news_images/featured_images/a3e8ea56bad0.jpeg&w=675"
+     name="Daniyal Saleem" caption="Pakistan Squad for T20 World Cup 2022"
+    ></Post>
+   
+   </>
+
+
+    // <Post image1=""></Post>
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
